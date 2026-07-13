be8ad2692d2bb8cb201615915cfe0a6899b13324
Melo-art-coder <MelodyKamto@gmail.com>
2026-07-13 12:26:01 +0100

    Update service worker cache name and files
    Updated cache name to 'melosave-v2' and modified cached files list.

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,35 @@
-const cacheName = "melosave-v1";
+const cacheName = "melosave-v2";
+
 
 const filesToCache = [
-"index.html",
-"style.css",
-"script.js",
-"signup.html",
-"login.html",
-"dashboard.html",
-"profile.html"
+
+    "index.html",
+    "signup.html",
+    "login.html",
+    "dashboard.html",
+    "profile.html",
+    "style.css",
+    "script.js",
+    "mani.json"
+
 ];
+
 
 
 self.addEventListener(
 "install",
 event => {
 
-event.waitUntil(
+    event.waitUntil(
 
-caches.open(cacheName)
-.then(cache => {
+        caches.open(cacheName)
+        .then(cache => {
 
-return cache.addAll(filesToCache);
+            return cache.addAll(filesToCache);
 
-})
+        })
 
-);
+    );
 
 });
 
@@ -35,6 +40,19 @@ return cache.addAll(filesToCache);
 self.addEventListener(
 "fetch",
 event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});event => {
 
 event.respondWith(
 
